caac6afe27c46b8fdbb9d108d3aa402b0878d934
David Rubin <david.rubin95@gmail.com>
2022-12-04 09:27:25 +0100

    feat: day 4 solved

---
+++ 2022/4/main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	_ "embed"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+//go:embed challenge.in
+var input string
+
+type ElfCleaner struct {
+	sectionMin int
+	sectionMax int
+}
+
+func (a ElfCleaner) overlaps(b ElfCleaner) bool {
+	if a.sectionMax >= b.sectionMin && a.sectionMin <= b.sectionMin ||
+		a.sectionMin <= b.sectionMin && a.sectionMax >= b.sectionMax {
+		return true
+	}
+	return false
+}
+
+func (a ElfCleaner) fullyContains(b ElfCleaner) bool {
+	if a.sectionMin <= b.sectionMin && a.sectionMax >= b.sectionMax {
+		return true
+	}
+	return false
+}
+
+func parseAssignments(assignmentDesc string) []ElfCleaner {
+	sections := strings.Split(assignmentDesc, ",")
+	cleaners := make([]ElfCleaner, len(sections))
+	for i, section := range sections {
+		bounds := strings.Split(section, "-")
+		cleaners[i].sectionMin, _ = strconv.Atoi(bounds[0])
+		cleaners[i].sectionMax, _ = strconv.Atoi(bounds[1])
+	}
+	return cleaners
+}
+
+func isFullyContained(cleaners []ElfCleaner, n int) bool {
+	for i, elf := range cleaners {
+		if i == n {
+			continue
+		}
+		if cleaners[n].fullyContains(elf) {
+			return true
+		}
+	}
+	return false
+}
+
+func hasOverlap(cleaners []ElfCleaner, n int) bool {
+	for i, elf := range cleaners {
+		if i == n {
+			continue
+		}
+
+		if cleaners[n].overlaps(elf) {
+			return true
+		}
+	}
+	return false
+}
+
+func runChallenge(challengePart int) int {
+	contained := 0
+	for _, assignment := range strings.Split(input, "\n") {
+		cleaners := parseAssignments(assignment)
+
+		for i := range cleaners {
+			if challengePart == 1 && isFullyContained(cleaners, i) {
+				contained++
+				// If the cleaners have the same ranges it counts as 1 contain, not 2 ( 15-20,15-20 => 1 overlap not 2)
+				break
+			} else if challengePart == 2 && hasOverlap(cleaners, i) {
+				contained++
+				break
+			}
+		}
+	}
+	return contained
+}
+
+func main() {
+	answer := runChallenge(2)
+	fmt.Println(answer)
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/rubinda/aoc
 
 go 1.19
 
-require golang.org/x/exp v0.0.0-20221126150942-6ab00d035af9 // indirect
+require golang.org/x/exp v0.0.0-20221126150942-6ab00d035af9
